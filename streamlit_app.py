5cffb44dca93b05a28ffb68c4f76cba21b48ba73
Sampath Anne <sampathanne.sf@gmail.com>
2022-05-03 09:57:27 -0500

    Update streamlit_app.py
    Added header and text.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,9 @@
 import streamlit
 
 streamlit.title('My Mom\'s new healthy Diner')
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
