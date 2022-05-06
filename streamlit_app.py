d9c9c086523cfab0cdba2046b0b7049d25e27229
Sampath Anne <sampathanne.sf@gmail.com>
2022-05-06 13:28:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ try:
     #fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
     #fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
          streamlit.dataframe(back_from_function)
-Except URLError as e:
+except URLError as e:
     streamlit.error()
 #streamlit.write('The user entered', fruit_choice)
 
